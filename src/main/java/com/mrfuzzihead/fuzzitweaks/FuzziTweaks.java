b280d25efe58e4c29c61499e5c3efbd6af90db04
Duncan Campbell <campbellduncan12@gmail.com>
2026-01-15 16:00:27 -0600

    we don't need it client side

--- src/main/java/com/mrfuzzihead/fuzzitweaks/FuzziTweaks.java
+++ src/main/java/com/mrfuzzihead/fuzzitweaks/FuzziTweaks.java
@@ -7,7 +7,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = FuzziTweaks.MODID, version = Tags.VERSION, name = "FuzziTweaks", acceptedMinecraftVersions = "[1.7.10]")
+@Mod(modid = FuzziTweaks.MODID, version = Tags.VERSION, name = "FuzziTweaks", acceptedMinecraftVersions = "[1.7.10]", acceptableRemoteVersions = "*", useMetadata = true)
 public class FuzziTweaks {
 
     public static final String MODID = "fuzzitweaks";
