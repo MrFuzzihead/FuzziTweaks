50e33d2061ecf97aa903f48e334e1e92b0c10a10
Duncan Campbell <campbellduncan12@gmail.com>
2026-01-16 00:05:35 -0600

    Spotless Apply

--- src/main/java/com/mrfuzzihead/fuzzitweaks/FuzziTweaks.java
+++ src/main/java/com/mrfuzzihead/fuzzitweaks/FuzziTweaks.java
@@ -7,7 +7,13 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = FuzziTweaks.MODID, version = Tags.VERSION, name = "FuzziTweaks", acceptedMinecraftVersions = "[1.7.10]", acceptableRemoteVersions = "*", useMetadata = true)
+@Mod(
+    modid = FuzziTweaks.MODID,
+    version = Tags.VERSION,
+    name = "FuzziTweaks",
+    acceptedMinecraftVersions = "[1.7.10]",
+    acceptableRemoteVersions = "*",
+    useMetadata = true)
 public class FuzziTweaks {
 
     public static final String MODID = "fuzzitweaks";
