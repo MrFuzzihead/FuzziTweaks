fb5def8a4aa826e05cb198862f15bb5a46dce63e
Duncan Campbell <campbellduncan12@gmail.com>
2026-06-21 10:45:37 -0500

    Massive refactors

--- src/main/java/com/mrfuzzihead/fuzzitweaks/mixins/EarlyMixinsLoader.java
+++ src/main/java/com/mrfuzzihead/fuzzitweaks/mixins/EarlyMixinsLoader.java
@@ -1,11 +1,15 @@
 package com.mrfuzzihead.fuzzitweaks.mixins;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import net.minecraft.launchwrapper.Launch;
+
 import com.gtnewhorizon.gtnhmixins.IEarlyMixinLoader;
 import com.gtnewhorizon.gtnhmixins.builders.IMixins;
+import com.mrfuzzihead.fuzzitweaks.Config;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
@@ -45,6 +49,11 @@ public class EarlyMixinsLoader implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
     @Override
     public List<String> getMixins(Set<String> loadedCoreMods) {
+        // Load config here so that setApplyIf conditions read values from the .cfg file
+        // rather than the hard-coded field defaults. getMixins() is called before
+        // FMLPreInitializationEvent, so we must resolve the config file path ourselves
+        // using Launch.minecraftHome which is set by LaunchWrapper very early.
+        Config.synchronizeConfiguration(new File(Launch.minecraftHome, "config/fuzzitweaks.cfg"));
         return IMixins.getEarlyMixins(Mixins.class, loadedCoreMods);
     }
 }
